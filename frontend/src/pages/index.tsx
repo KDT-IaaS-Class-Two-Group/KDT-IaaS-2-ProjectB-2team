c6bccc9355b3cf9f591e083679f9f670e71877f3
jojayeon <jojayeon6152@gmail.com>
2024-09-25 17:26:45 +0900

    클릭시 fetch통신

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,27 +1,61 @@
-// pages/hello.tsx
-import { GetServerSideProps } from 'next';
+import { useState, useEffect } from "react";
 
-interface HelloProps {
-  message: string;
+
+interface Response {
+  Hello: string;
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  // 백엔드 서버에서 /data 엔드포인트로 GET 요청
-  const res = await fetch('http://127.0.0.1:8000/');
-  const data = await res.json();
+const Hello = () => {
+  const [message,setMessage] = useState<Response>({Hello : ""})
 
-  return {
-    props: {
-      message: data.message, // 백엔드에서 가져온 데이터를 props로 전달
-    },
-  };
+  useEffect(()=>{
+    const fetchData = async()=>{
+      try {
+        const res = await fetch('http://127.0.0.1:8000/');
+        const data = await res.json()
+        setMessage(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  },[]);
+  
+// 클릭 이벤트가 발생하면 콘솔에 숫자 1이 나타나게 
+// id = "root"안에 있는 첫번째 div의 id를 찾아서 나타나게 
+// 이제 이 id값을 fetch로 위에 http://127.0.0.1:8000으로 보내는 코드를 만든다.
+const eventhandle = async () => {
+  console.log("1");
+  const killingFieldDiv = document.getElementById('killingfield');
+  
+  if (killingFieldDiv) {
+    const idValue = killingFieldDiv.id;
+    console.log(idValue); // "killingfield"를 콘솔에 출력합니다.
+
+    // GET 요청 보내기
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/${idValue}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const responseData = await response.json(); // 응답 데이터 받아오기
+      console.log('Response from server:', responseData);
+      console.log(responseData.message)
+    } catch (error) {
+      console.log('Error sending id:', error);
+    }
+  }
 };
-
-const Hello = ({ message }: HelloProps) => {
   return (
     <div id="root">
       <div id="killingfield">
-        <h1>Hello, {message}</h1>
+        <h1>Hello, {message.Hello ? message.Hello : "fail" }, {}</h1>
+        <div>
+          <button type="button" onClick={eventhandle}>버튼</button>
+        </div>
       </div>
     </div>
   );
