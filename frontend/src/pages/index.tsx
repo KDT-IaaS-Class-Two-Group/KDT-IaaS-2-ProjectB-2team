a749303b4ff63b95940e6cc5724791d3b2de40f3
jojayeon <jojayeon6152@gmail.com>
2024-09-25 18:18:28 +0900

    버튼클릭시 문장 변경

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,11 +2,14 @@ import { useState, useEffect } from "react";
 
 
 interface Response {
-  Hello: string;
+  Hello: string,
+  message? :string
 }
 
 const Hello = () => {
   const [message,setMessage] = useState<Response>({Hello : ""})
+
+  console.log(message)
 
   useEffect(()=>{
     const fetchData = async()=>{
@@ -20,12 +23,17 @@ const Hello = () => {
     }
     fetchData()
   },[]);
+
+
   
 // 클릭 이벤트가 발생하면 콘솔에 숫자 1이 나타나게 
 // id = "root"안에 있는 첫번째 div의 id를 찾아서 나타나게 
 // 이제 이 id값을 fetch로 위에 http://127.0.0.1:8000으로 보내는 코드를 만든다.
+
 const eventhandle = async () => {
+
   console.log("1");
+
   const killingFieldDiv = document.getElementById('killingfield');
   
   if (killingFieldDiv) {
@@ -44,15 +52,21 @@ const eventhandle = async () => {
       const responseData = await response.json(); // 응답 데이터 받아오기
       console.log('Response from server:', responseData);
       console.log(responseData.message)
+      setMessage(responseData)
+
     } catch (error) {
       console.log('Error sending id:', error);
     }
   }
+  
 };
+
+
+
   return (
     <div id="root">
       <div id="killingfield">
-        <h1>Hello, {message.Hello ? message.Hello : "fail" }, {}</h1>
+        <h1>Hello, {message.message ? message.message : message.Hello }</h1>
         <div>
           <button type="button" onClick={eventhandle}>버튼</button>
         </div>
