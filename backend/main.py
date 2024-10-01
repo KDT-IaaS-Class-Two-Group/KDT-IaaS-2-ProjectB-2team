8f4a06932e4099e0064ac750651b7987137030cd
조우식 <chousik01@gmail.com>
2024-10-01 17:24:48 +0900

    feat | main.py
    fastAPI 서버에서 모델 파일을 읽어오고, 바이너리 데이터로 전송된 이미지를 처리하는 로직

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from modules.FastAPI_preprocess import model_predict
+from modules.load_model import load_model
 import uvicorn
 import json
 import os
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -13,16 +15,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# 현재 파일이 실행되는 디렉토리 경로 가져오기
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
-
-# 'data' 폴더 하위의 'data.json' 파일 경로 동적으로 생성
 file_path = os.path.join(current_dir, 'data', 'data.json')
 
-# 파일 열기
 with open(file_path, 'r', encoding='utf-8') as file:
     data = json.load(file)  # JSON 파일을 파싱해서 Python 객체로 변환
+
+
 
 
 @app.get("/")
@@ -32,8 +31,24 @@ def read_root():
 @app.get("/{get_end_point}")
 def dynamic_get(get_end_point: str):
     return {"message": f"Hello from {get_end_point}"}
+
+
+
+model = load_model("./model/M5.keras")
+
+@app.post("/predict")
+async def predict_response(file: UploadFile = File(...)):
+    print("test")
+    result = await model_predict(file,model)
+    return result
+        
+    
+
 @app.post("/{post_end_point}")
 def dynamic_get(post_end_point: str):
     return {"message",data}
+
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
