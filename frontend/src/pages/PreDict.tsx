ad3b8f8365741d4ec1aa773446a28f72f954fa98
jojayeon <jojayeon6152@gmail.com>
2024-10-13 01:17:33 +0900

    Style: PreDict.tsx
    스타일 수정

--- frontend/src/pages/PreDict.tsx
+++ frontend/src/pages/PreDict.tsx
@@ -67,41 +67,51 @@ const Predict: React.FC  = () => {
     return (
       <div id="root" className="bg-gray-200 p-4 flex flex-col">
         <div className="text-xl font-bold mb-4 flex justify-center">사망 보고서</div>
-        <div className="flex mb-4 bg-gray-400 p-2">
-          <div className="w-1/4 bg-gray-500 flex justify-center items-center text-white">
+        <div className="flex mb-4 bg-gray-400 p-2 rounded">
+          <div className="w-1/4 bg-gray-200 flex justify-center items-center mr-5">
             이미지 : {data.img}
           </div>
-          <div className="w-3/4 bg-gray-200 flex">
-            <div className="w-3/4 p-2">
-              <div className="flex justify-evenly mb-2">
-                <div className="bg-gray-300 p-1">닉네임 : {nickname}</div>
-                <div className="bg-gray-300 p-1">지역 : {region}</div>
+          <div className="w-3/4 bg-gray-200 flex p-2">
+            <div className="w-3/4 p-2 flex flex-col">
+              <div className="flex mb-2 ">
+                <div className="flex w-1/2 border-white border-2">
+                  <div className="flex w-1/2  p-1 justify-center mt-1">닉네임 </div>
+                  <div className="flex w-1/2 bg-gray-400 p-1 m-1 justify-center">{nickname}</div>
+                </div>
+                <div className="flex w-1/2 border-white border-2">
+                  <div className="flex w-1/2  p-1 justify-center mt-1">지역 </div>
+                  <div className="flex w-1/2 bg-gray-400 p-1 m-1 justify-center">{nickname}</div>
+                </div>
               </div>
-              <div className="flex">
-                <div className="bg-gray-300 p-2 w-1/4">능력치</div>
-                <div className="grid grid-cols-2 gap-1 w-3/4 bg-gray-100 p-2">
-                  <div>체력 : 100</div>
-                  <div>공격력: {data.attack}</div>
-                  <div>방어력: {data.defense}</div>
-                  <div>정확도: {data.accuracy}</div>
-                  <div>민첩성: 없는디 머여?</div>
-                  <div>무게: {data.weight}</div>
+              <div className="flex border-white border-2">
+                <div className="p-2 w-1/4 flex justify-center items-center text-2xl">능력치</div>
+                <div className="grid grid-cols-3 gap-1 w-3/4 p-2">
+                  <div className="bg-gray-400 justify-center flex">체력 : 100</div>
+                  <div className="bg-gray-400 justify-center flex">공격력: {data.attack}</div>
+                  <div className="bg-gray-400 justify-center flex">방어력: {data.defense}</div>
+                  <div className="bg-gray-400 justify-center flex">정확도: {data.accuracy}</div>
+                  <div className="bg-gray-400 justify-center flex">민첩성: 없슈</div>
+                  <div className="bg-gray-400 justify-center flex">무게: {data.weight}</div>
                 </div>
               </div>
             </div>
-            <div className="w-1/4 flex justify-center items-center text-white bg-gray-600 text-xl text-6xl">10일
+            <div className="w-1/4 flex justify-center items-center text-white bg-gray-400 text-6xl">10일
             </div>
           </div>
         </div>
     
-        <div className="text-lg font-bold mb-2">사망 이력</div>
-        <ul className="bg-white p-2 mb-4 border border-gray-400">
-          {[...Array(6)].map((_, i) => (
-            <li key={i} className="mb-1 border-b border-gray-300 py-2 px-2">
-              Day {i + 1}: 내용
-            </li>
-          ))}
-        </ul>
+        <div className="text-lg font-bold bg-gray-400 w-1/3 flex justify-center rounded-t">사망 이력</div>
+        <div className="bg-gray-400">
+          <div className="bg-white m-2 pt-1 pb-1">
+            <ul className=" p-2 m-2">
+              {[...Array(6)].map((_, i) => (
+                <li key={i} className="py-2 px-2  bg-gray-400 m-2 p-2 text-white">
+                  Day {i + 1}: 내용
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
         <div>
         <button onClick={handleSubmit} className="bg-blue-500 text-white py-1 px-4 rounded mt-2">다시 시작</button>
         </div>
