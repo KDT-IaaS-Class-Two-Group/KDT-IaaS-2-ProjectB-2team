4431ba5954ce397e426043fc61eb783f81ee3173
fbghk <im706374@gmail.com>
2024-10-13 04:56:50 +0900

    Fix | Home.tsx
    tailwind 사용하여 구조 수정

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,15 +6,16 @@ const MainPage: React.FC = () => {
   };
 
   return (
-    <div id="root" className="h-screen grid place-items-center text-center">
-      <div>
-        <h1 className="text-3xl font-bold mb-4">Zom 이상하다고 생각은 했지만<br/>Bi 상사태가 발생했습니다.</h1>
+    <div id="root" className="w-[812px] h-[1080px] bg-[#D9D9D9] grid place-items-center mx-auto my-auto">
+      <div className="text-[38px] grid place-items-center absolute top-[314px] left-1/2 transform -translate-x-1/2">
+        <h1>Zom 이상하다고 생각은 했지만<br/></h1>
+        <h1>Bi 상사태가 발생했습니다.</h1>
+      </div>
+      <div className="w-[743px] h-[23px] text-[18px] grid place-items-center absolute top-[521px] left-1/2 transform -translate-x-1/2">
+        <p>이미지분석시뮬레이션모델 머신러닝 투 좀비 이거 보여주려고 어그로 끌었다 좀비 실화냐?</p> 
       </div>
       <div>
-        <p className="text-lg mb-6">이미지분석시뮬레이션모델 머신러닝 투 좀비 이거 보여주려고 어그로 끌었다 좀비 실화냐?</p> 
-      </div>
-      <div>
-        <button onClick={handleStartClick} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">시작</button>
+        <button onClick={handleStartClick} className="w-[150px] h-[50px] bg-[#1E1E1E] text-white grid place-items-center absolute top-[700px] left-1/2 transform -translate-x-1/2">시작</button>
       </div>
     </div>
     
