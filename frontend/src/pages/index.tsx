7c5a1526515b2a20e43125d9c19ef31496bac5da
fbghk <im706374@gmail.com>
2024-10-12 20:41:37 +0900

    New | index.tsx
    tailwind 사용을 위해 추가
    Home.tsx 만 반환

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,23 +1,9 @@
 import React from 'react';
+import './index.css'; // Tailwind CSS import
+import Home from './Home'; // Home.tsx
 
-const MainPage: React.FC = () => {
-  const handleStartClick = () => {
-    window.location.href = '/UserPage';
-  };
-
-  return (
-    <div id="root">
-      <div>
-        <h1>Zom 이상하다고 생각은 했지만<br/>Bi 상사태가 발생했습니다.</h1>
-      </div>
-      <div>
-        <p>이미지분석시뮬레이션모델 머신러닝 투 좀비 이거 보여주려고 어그로 끌었다 좀비 실화냐?</p> 
-      </div>
-      <div>
-        <button onClick={handleStartClick}>시작</button>
-      </div>
-    </div>
-  );
+const HomePage: React.FC = () => {
+  return <Home />;
 };
 
-export default MainPage;
+export default HomePage;
