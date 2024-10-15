3cadc396f4b8d7fbbb0feb9d80992b50baa485c2
fbghk <im706374@gmail.com>
2024-10-15 19:05:08 +0900

    Npm | tailwind.config.ts
    cfont: ['cfont', 'sans-serif']
    customfont 추가

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        cfont: ['cfont', 'sans-serif'],
+      },
     },
   },
   plugins: [],
