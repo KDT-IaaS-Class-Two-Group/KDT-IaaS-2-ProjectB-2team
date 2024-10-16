f5493c73f609ecb8db00d421d0bb800b8c359112
조우식 <chousik01@gmail.com>
2024-10-16 17:31:16 +0900

    feat | _app.tsx
    page 라우팅 방식에서 layout.tsx 역할을 하는 _app 파일에 globals.css 주입

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { UserProvider } from '@/components/context';
+import "../../public/globals.css"
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
