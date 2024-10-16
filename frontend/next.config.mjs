a2fd5f40e26fa89240d74ff39ade99a48cb42699
조우식 <chousik01@gmail.com>
2024-10-16 17:27:23 +0900

    feat | next.config.mjs
    CORS 발생 할 경우에 대한 대응

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/fonts/:font*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
