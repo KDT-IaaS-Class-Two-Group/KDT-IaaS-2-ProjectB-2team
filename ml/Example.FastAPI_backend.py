3ef3cccb8b945e69b9fe53c51d5b8c16042288a6
조우식 <chousik01@gmail.com>
2024-10-21 17:32:09 +0900

    path | examples
    예시를 제작하기 위해 작성했던 코드를 루트 디렉토리로 이동합니다.
    시인성을 위해 파일명 서두에 example을 추가했습니다.

--- ml/Example.FastAPI_backend.py
+++ ml/Example.FastAPI_backend.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from modules.model_loader import load_model
-from modules.FastAPI_preprocess import model_predict  # model_predict 함수를 import
+from modules.FastAPI_preprocess import model_predict
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -15,7 +15,7 @@ app.add_middleware(
 )
 
 # 학습된 모델을 앱 시작 시 한 번만 로드
-model = load_model("./model/M5.keras")
+model = load_model("../models/M5.keras")
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
