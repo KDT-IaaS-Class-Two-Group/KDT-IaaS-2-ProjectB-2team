2dcac46525248aa6d9fb05622e61e3757ceeae02
saccharin01 <chousik01@gmail.com>
2024-11-01 21:08:18 +0900

    feat | index.tsx
    랜딩페이지의 레이아웃 조정 중

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React from "react";
 // import '../app/globals.css';
-import { useRouter } from 'next/router';
-
+import { useRouter } from "next/router";
 const MainPage: React.FC = () => {
-const router = useRouter()
+  const router = useRouter();
 
   const handleStartClick = () => {
-      router.push("/UserPage")
+    router.push("/predict");
   };
 
   return (
-    <div id="root" className="flex flex-col items-center justify-center h-screen ">
-      <div className="relative mb-4">
-      <img src="/images/index.png" alt="description" className="w-1080 h-screen object-cover" />
-      <button onClick={handleStartClick}
-      className="absolute top-[787px] left-[611px] 
+    <div
+      className="flex flex-col items-center justify-center h-screen mx-40"
+    >
+
+      <div className="indexImage w-4/5 h-4/5 min-h-64 min-w-64">
+
+      </div>
+      <div className="mb-4">
+        <button
+          onClick={handleStartClick}
+          className=" 
       px-4 py-2 bg-[#332F47CC] rounded border border-[#D9C4B2] 
-      font-cfont text-[#C5C1C3] text-lg hover:bg-[#D9C4B2CC] hover:text-black">
-        게임 시작
-      </button>
+      font-cfont text-[#C5C1C3] text-lg hover:bg-[#D9C4B2CC] hover:text-black"
+        >
+          게임 시작
+        </button>
       </div>
     </div>
   );
