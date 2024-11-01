896621d9d78500f6836938e72c775e8dd3726993
saccharin01 <chousik01@gmail.com>
2024-11-01 19:46:08 +0900

    feat | Image_model_predict.py
    이미지 모델에서 예측값을 추출해내는 함수를 조정합니다. 이제 현 프로젝트에서 사용되는 텐서플로우는 제거되고, ONNX 런타임으로 대체되어 경량화를 노릴 수 있습니다.

--- backend/modules/Image_model_predict.py
+++ backend/modules/Image_model_predict.py
@@ -1,7 +1,10 @@
-import tensorflow as tf
+import onnxruntime as ort
+from PIL import Image
+import numpy as np
 from typing import Dict
 from fastapi import HTTPException
 from modules.static.class_list import class_list
+import io
 
 """
 FastAPI 기반 비동기 응답에 대응하는 함수입니다.
@@ -26,25 +29,28 @@ FastAPI 기반 비동기 응답에 대응하는 함수입니다.
     model_loader 함수를 이용해 모델의 반환값을 받아냅니다.
 """
 
-
-async def model_predict(img_data: bytes, model) -> Dict[str, float]:
+async def model_predict(img_data: bytes, model: ort.InferenceSession) -> Dict[str, int]:
     try:
-        image = tf.image.decode_image(img_data, channels=3) 
-        image = tf.image.resize(image, [128, 256])
-        image = tf.expand_dims(image, axis=0)
-        image = image / 255.0
+        # 이미지 전처리
+        image = Image.open(io.BytesIO(img_data)).convert("RGB")
+        image = image.resize((256, 128))  # 모델에 맞게 입력 크기 조정 (필요에 따라 조정 가능)
+        image_array = np.array(image).astype("float32") / 255.0
+        image_array = np.expand_dims(image_array, axis=0)  # 배치 차원 추가, (1, 128, 256, 3)
 
-        # 3. 모델 예측
-        predictions = model.predict(image)
+        # ONNX 모델 예측
+        input_name = model.get_inputs()[0].name
+        predictions = model.run(None, {input_name: image_array})[0]
 
+        # 예측 결과를 dict로 정리
         response = {}
-        for i in range(len(class_list)):
-            pred_value = predictions[0][i]
+        for i, class_name in enumerate(class_list):
+            pred_value = predictions[0][i]  # 2차원 배열 접근 방식 사용
 
-            if class_list[i] == 'species':
-                response['species'] = 1 if pred_value >= 0.5 else 0
+            # 값 변환 및 반올림 후 정수형으로 변환
+            if class_name == "species":
+                response["species"] = 1 if pred_value >= 0.5 else 0
             else:
-                response[class_list[i]] = int(round(pred_value, 1))
+                response[class_name] = int(round(pred_value))
 
         return response
 
