609090daa449fb7760ae32bdaeccfad5bad9d6de
saccharin01 <chousik01@gmail.com>
2024-11-02 19:35:03 +0900

    feat | index.tsx
    메인 화면 레이아웃 구성을 바꿔야 할 지도 모름

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -10,25 +10,28 @@ const MainPage: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen mx-4 sm:mx-8 lg:mx-40">
-      <div className="relative w-full max-w-lg h-64 sm:h-72 md:h-96 lg:h-[500px] xl:h-[600px] min-h-[300px]">
+    <>
+      <div className="h-screen relative">
         <Image
           src="/images/Title.png"
           alt="Title"
           fill
-          sizes="(max-width: 640px) 100vw, (max-width: 768px) 75vw, (max-width: 1024px) 50vw, 33vw"
           style={{ objectFit: "contain" }}
         />
         <button
           onClick={handleStartClick}
-          className="absolute bottom-4 sm:bottom-6 md:bottom-8 lg:bottom-10 left-1/2 transform -translate-x-1/2 
-                     px-3 sm:px-4 md:px-5 py-1 sm:py-2 md:py-3 bg-[#332F47CC] rounded border border-[#D9C4B2]
-                     font-cfont text-sm sm:text-base md:text-lg lg:text-xl text-[#C5C1C3] hover:bg-[#D9C4B2CC] hover:text-black"
+          className="absolute bottom-[20%] right-[38%]
+             px-5 py-3 bg-[#332F47CC] rounded border border-[#D9C4B2]
+             font-cfont text-lg text-[#C5C1C3] hover:bg-[#D9C4B2CC] hover:text-black"
+          style={{
+            bottom: "20%",
+            right: "calc(38% - 50px)",
+          }}
         >
           게임 시작
         </button>
       </div>
-    </div>
+    </>
   );
 };
 
