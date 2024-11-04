b5870d6a547c47c9a60dd969816e5d20bb87c03d
saccharin01 <chousik01@gmail.com>
2024-11-04 22:19:26 +0900

    feat | index.tsx
    랜딩페이지 레이아웃 수정 완료
    반응형 구현을 위해 테일윈드의 미디어 쿼리 기반의 선택자 이용
    
    정적 이미지의 공백을 제거

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -12,19 +12,19 @@ const MainPage: React.FC = () => {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
       {/* 이미지 영역 */}
-      <div className="relative flex items-center justify-center">
+      <div className="relative flex items-center justify-center w-full max-w-[1000px]">
         <Image
           src="/images/Title.png"
           alt="Title"
-          width={500} // 이미지 너비
-          height={300} // 이미지 높이
+          width={573} // 원하는 기본 너비 (1000px로 확대)
+          height={532} // 원하는 기본 높이
           priority
-          className="h-[60rem] w-[60rem] sm:max-w-[600px] sm:h-3/4 sm:w-full lg:w-full"
+          className="object-contain" // 비율 유지 및 반응형 적용
         />
       </div>
 
       {/* 버튼 영역 */}
-      <div className="w-2/5 flex justify-end">
+      <div className="w-4/5 flex justify-end mt-4 md:w-[35%]">
         <button
           onClick={handleStartClick}
           className="px-5 py-3 bg-[#332F47CC] rounded border border-[#D9C4B2]
