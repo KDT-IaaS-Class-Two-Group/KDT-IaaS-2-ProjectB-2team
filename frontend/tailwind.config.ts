3cdf88d3aac92b02e77cb057c8460fb635300a22
saccharin01 <chousik01@gmail.com>
2024-11-05 21:12:35 +0900

    feat | tailwind.config.ts
    테일윈드 컨피그에 커스텀 브레이크 포인트 추가
    fhd
    1920 해상도에서 적용될 스타일을 브레이크 포인트로 추가합니다

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        'fhd': '1920px',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
