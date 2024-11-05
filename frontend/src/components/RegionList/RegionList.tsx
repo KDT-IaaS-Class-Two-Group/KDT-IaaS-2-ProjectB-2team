8fb2fbdc1a88c438f865578a6efd0c64f34c1dc0
saccharin01 <chousik01@gmail.com>
2024-11-05 23:44:02 +0900

    path | RegionList.tsx
    정적 데이터에 따라 지역 리스트 옵션을 반환하는 컴포넌트의 위치를 변경

--- frontend/src/components/RegionList/RegionList.tsx
+++ frontend/src/components/RegionList/RegionList.tsx
@@ -1,19 +1,16 @@
 import { regionList } from "@/shared/static.regionList";
+import RegionListProps from "./RegionList.interface";
 import React from "react";
 
-interface RegionListProps {
-  selectedOption: string;
-  onSelectChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
-}
 
-const RegionList: React.FC<RegionListProps> = ({ selectedOption, onSelectChange }) => {
+
+const RegionList: React.FC<RegionListProps> = ({ selectRef }) => {
   return (
     <div className="w-full flex justify-around sm:justify-start bg-[#332F47CC] rounded border border-[#D9C4B2] font-cfont">
       <label htmlFor="region" className="text-[#C5C1C3] font-cfont">지역 : </label>
       <select
         id="region"
-        value={selectedOption}
-        onChange={onSelectChange}
+        ref={selectRef}  // 부모로부터 전달된 ref를 select 요소에 연결
         className="bg-[#332F47CC] text-[#C5C1C3]"
       >
         <option value="선택" className="font-cfont text-[#C5C1C3] text-lg">선택하세요</option>
