d911c43d0bb3402caff735bce5048434db771863
saccharin01 <chousik01@gmail.com>
2024-12-02 21:31:09 +0900

    feat | predict.tsx
    사용자의 입력값을 받아내는 페이지에서 사용되는 인터페이스를 분리 및 코드 스타일 조정.
    정적으로 박혀있던 DOM 요소를 컴포넌트 단위로 분리

--- frontend/src/pages/predict.tsx
+++ frontend/src/pages/predict.tsx
@@ -1,132 +1,61 @@
-import React, { useContext } from "react";
-// import '../app/globals.css';
+import React from "react";
 import { useUserContext } from "@/components/hooks/useUserContext";
+import StatCard from "@/components/statCard/StatCard";
 
 const Predict: React.FC = () => {
-  const data = useUserContext(); // UserContext에서 데이터 받아오기
-  if (!data) {
-    throw new Error("UserContext must be used within a UserProvider");
-  }
-
-  const { userData } = data;
-  console.log(userData?.result);
-  // userData?.result
-
-  const { nickname, region, img, stat, log } = userData?.result || {
-    nickname: "",
-    region: "",
-    img: "",
-    stat: {
-      species: 0,
-      attack: 0,
-      defense: 0,
-      accuracy: "",
-      weight: 0,
-    },
-    log: [],
-  };
+  const { userData } = useUserContext(); // useUserContext에서 userData를 가져옵니다.
 
   const handleSubmit = () => {
     window.location.href = "/";
   };
 
   return (
-    <div className="p-4 flex flex-col items-center justify-center">
-        <img
-          src="/images/predict.png"
-          alt="description"
-          className="w-1080 min-h-1075 object-cover"
-        />
-        <div className="absolute top-0 inset-0 flex flex-col items-center justify-center ">
-          <div className="flex bg-[#332F47CC] p-2 rounded border border-[#D9C4B2] w-[464px] h-[120px]">
-            <div className=" flex justify-center items-center mr-2">
-              <div className="relative w-[85px] h-[102px] overflow-hidden">
-                <img
-                  src={img}
-                  alt="Uploaded"
-                  className="object-cover w-full h-full"
-                />
-              </div>
-            </div>
-
-            <div className="w-5/6 bg-[#332F47CC] flex">
-              <div className="w-3/4 flex flex-col items-center">
-                <div className="flex mb-2 space-x-1">
-                  <div className="flex w-[97px] h-[22px] border-[#D9C4B2] border-2 items-center">
-                    <div className="flex w-1/2 justify-center items-center text-[#C5C1C3] text-[8px] text-center">
-                      닉네임 :
-                    </div>
-                    <div className="flex w-1/2 justify-center items-center text-[#C5C1C3] text-[8px] text-center">
-                      {nickname}
-                    </div>
-                  </div>
-                  <div className="flex w-[97px] h-[22px] border-[#D9C4B2] border-2 ">
-                    <div className="flex w-1/2 justify-center items-center text-[#C5C1C3] text-[8px] text-center">
-                      지역:
-                    </div>
-                    <div className="flex w-1/2 bg-[#332F47CC] justify-center items-center text-[#C5C1C3] text-[8px] text-center">
-                      {region}
-                    </div>
-                  </div>
+    <div className="p-4 flex flex-col items-center justify-center relative">
+      <div className="absolute top-0 inset-0 flex flex-col items-center justify-center">
+        <div className="flex bg-[#332F47CC] p-2 rounded border border-[#D9C4B2] w-[464px] h-[120px]">
+          <div className="flex justify-center items-center mr-2">
+            <div className="relative w-[85px] h-[102px] overflow-hidden">
+              {userData?.img && (
+                <div className="flex justify-center items-center bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2] border-2 text-[40px]">
+                  <img
+                    src={`data:image/png;base64,${userData.img}`}
+                    alt="Uploaded"
+                    className="object-cover w-full h-full"
+                  />
                 </div>
-                <div className="flex border-[#D9C4B2] w-[198px] h-[72px] border-2">
-                  <div className="p-2 w-1/4 flex justify-center items-center text-[9px] text-center text-[#C5C1C3]">
-                    능력치
-                  </div>
-                  <div className="grid grid-cols-3 gap-1 w-3/4 p-1">
-                    <div className="bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2] border-2 flex justify-center items-center text-center text-[7px]">
-                      체력 : 100
-                    </div>
-                    <div className="bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2] border-2 flex justify-center items-center text-center text-[7px]">
-                      공격력 : {stat.attack}
-                    </div>
-                    <div className="bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2] border-2 flex justify-center items-center text-center text-[7px]">
-                      방어력 : {stat.defense}
-                    </div>
-                    <div className="bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2] border-2 flex justify-center items-center text-center text-[7px]">
-                      정확도 : {stat.accuracy}
-                    </div>
-                    <div className="bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2] border-2 flex justify-center items-center text-center text-[7px]">
-                      민첩성 : 10
-                    </div>
-                    <div className="bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2] border-2 flex justify-center items-center text-center text-[7px]">
-                      무게 : {stat.weight}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className=""> </div>
-              <div className="w-1/4 flex justify-center items-center bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2] border-2 text-[40px]">
-                {log.length}일
-              </div>
+              )}
             </div>
           </div>
 
-          <div className="mb-3" />
-          <div className="w-[464px] bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2] border-2 p-4 flex flex-col items-center">
-            <h2 className="text-[17px] font-bold mb-4">사망 이력</h2>
-            <div className="bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2] p-3 mb-4">
-            <ul className="list-disc space-y-2">
-  {log.map((entry, i) => (
-    <li
-      key={i}
-      className="py-2 px-4 bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2]"
-    >
-      {i + 1}일차 : {Object.values(entry).join(", ")}
-    </li>
-  ))}
-</ul>
-            </div>
-
-            <button
-              onClick={handleSubmit}
-              className="bg-blue-500 text-white py-2 px-4 rounded mt-2 hover:bg-blue-700"
-            >
-              다시 시작
-            </button>
+          <div className="w-5/6 bg-[#332F47CC] flex">
+            {userData && userData.stat && (
+              <StatCard
+                userData={{
+                  nickname: userData.nickname,
+                  region: userData.region,
+                }}
+                stat={userData.stat}
+              />
+            )}
           </div>
         </div>
+
+        <div className="mb-3" />
+        <div className="w-[464px] bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2] border-2 p-4 flex flex-col items-center">
+          <h2 className="text-[17px] font-bold mb-4">사망 이력</h2>
+          <div className="bg-[#332F47CC] text-[#C5C1C3] border-[#D9C4B2] p-3 mb-4">
+            {/* 여기에 사망 이력 목록 렌더링 코드 추가 */}
+          </div>
+          <button
+            onClick={handleSubmit}
+            className="bg-blue-500 text-white py-2 px-4 rounded mt-2 hover:bg-blue-700"
+          >
+            다시 시작
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
+
 export default Predict;
