fe049b717538fa44bc162e1903f9425dd9a92ef8
saccharin01 <chousik01@gmail.com>
2024-12-07 21:23:59 +0900

    feat | tailwind.config.ts
    커스텀 애니메이션 부분 추가

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,17 +9,31 @@ const config: Config = {
   theme: {
     extend: {
       screens: {
-        'fhd': '1920px',
+        fhd: "1920px",
       },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
       fontFamily: {
-        cfont: ['cfont', 'sans-serif'],
+        cfont: ["cfont", "sans-serif"],
+      },
+      /**
+       * 커스텀 애니메이션
+       * 생존 로그의 div에 마우스 오버 시 반짝이는 애니메이션을 추가
+       */
+      animation: {
+        blink: "blink 2s infinite", // 애니메이션 이름 및 설정
+      },
+      keyframes: {
+        blink: {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0.5" },
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;
